da0331708b48214ac9ffe1aea41cff49bd7711df
Nick Doutrich <nick@childpovertyactionlab.org>
2025-04-07 17:52:38 -0500

    updated words on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the updatd documentation
+</p>
 <p>Here is my API Key {import.meta.env.NASA_API_KEY}</p>
