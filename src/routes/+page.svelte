070a05ebafbca9d33dd666bddbdd7dc6e09fe160
Nick Doutrich <nick@childpovertyactionlab.org>
2025-04-07 17:53:15 -0500

    fixed typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the updatd documentation
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the updated documentation
 </p>
 <p>Here is my API Key {import.meta.env.NASA_API_KEY}</p>
