3575e69fdf59fc8a6476ce90a950c58778cc09be
Nick Doutrich <nick@childpovertyactionlab.org>
2025-04-07 17:49:24 -0500

    initialized mars-rover repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Here is my API Key {import.meta.env.NASA_API_KEY}</p>
